a2f5487420d01fe882f01617f85af315b1bfd923
shawn-dsilva <shawn.dsilva_97@protonmail.com>
2020-02-15 17:50:48 +0530

    cleanup

--- public/index.php
+++ public/index.php
@@ -1,20 +1,8 @@
 <?php
 
-// require_once '../models/Model.php';
 require '../utils/Router.php';
 require '../utils/helpers.php';
 
 
 
 Router::initRouter("../controller/RoutesController.php","../utils/methods.php")->handleRoute(findURI(),findMethod());
-
-// $data=array(
-//   'name' => 'abc',
-//   'email' => 'abc@xyz.com',
-//   'password' => '123456'
-// );
-
-// $model = new Model();
-
-
-// echo ($model->insertInto('user', $data));
