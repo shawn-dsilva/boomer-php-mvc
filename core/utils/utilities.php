2b990ac05329796c4c2a26d684ea3afa77ddea39
shawn-dsilva <shawn.dsilva_97@protonmail.com>
2020-02-29 17:36:41 +0530

    input checking functions added.

--- core/utils/utilities.php
+++ core/utils/utilities.php
@@ -11,3 +11,19 @@ function findMethod(){
 function getView($name, $data = []) {
   return require "../app/views/{$name}View.php";
 }
+
+function validateEmail($email) {
+  if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    return "E-Mail is not valid";
+  } else {
+    return $email;
+  }
+}
+
+function allowedCharacters($string, $name) {
+  if (!preg_match("/^[a-zA-Z0-9-_]+$/", $string)) {
+    return "Invalid {$name}, only Upper and lower case letters, Numbers, - and _ are allowed";
+  } else {
+    return $string;
+  }
+}
