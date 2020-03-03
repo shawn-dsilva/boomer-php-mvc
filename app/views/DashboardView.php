f658b758ddfa77d7c3df85f656468fa862a513d1
shawn-dsilva <shawn.dsilva_97@protonmail.com>
2020-03-03 10:08:04 +0530

    fixes

--- app/views/DashboardView.php
+++ app/views/DashboardView.php
@@ -4,5 +4,10 @@
 <?php
     echo "Welcome <b>{$data['name']}</b> ! <br>
     Your E-Mail is <b>{$data['email']}</b>   " ?>
-
+<a href="javascript:logout()">Logout</a>
+<script>
+function logout() {
+    document.cookie = "cookiename=sessionId ; expires = Thu, 01 Jan 1970 00:00:00 GMT"
+}
+</script>
 <?php require('includes/footer.html'); ?>
--- core/utils/Validator.php
+++ core/utils/Validator.php
@@ -6,7 +6,7 @@ class Validator {
 
   public function validateEmail($email) {
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      $this->errMsg = "E-Mail is not valid";
+      $this->errMsg = "E-Mail is not valid, Empty fields not allowed";
       return FALSE;
     } else {
       return TRUE;
@@ -15,7 +15,7 @@ class Validator {
 
   public function allowedCharacters($string, $name) {
     if (!preg_match("/^[a-zA-Z0-9-_]+$/", $string)) {
-      $this->errMsg = "Invalid {$name}, only Upper and lower case letters, Numbers, - and _ are allowed";
+      $this->errMsg = "Invalid {$name}, only Upper and lower case letters, Numbers, - and _ are allowed<br> Empty fields not allowed";
       return FALSE;
     } else {
       return TRUE;
@@ -31,6 +31,13 @@ class Validator {
         $this->allowedCharacters($value, $key) ?  TRUE : $this->errMsg;
       }
     }
+
+    if(empty($this->errMsg)) {
+      return TRUE;
+    } else {
+      return FALSE;
+    }
+
   }
 
   public function getErrMsg() {
