65978dcac75c8d9eb1979349b465e60ae68d7369
shawn-dsilva <shawn.dsilva_97@protonmail.com>
2020-03-19 21:18:37 +0530

    basic splash screen done

--- app/views/HomeView.php
+++ app/views/HomeView.php
@@ -4,11 +4,21 @@
   <a class="logo" href="#home">BOOMER</a>
 
   <div class="right-container">
-    <a class="right-text" href="https://www.github.com/shawn-dsilva/boomer-php-mvc">Source on Github</a>
-<a class="right-text" href="/login">Login.</a>
- <a class="right-text" href="/register">Register.</a>
+    <a class="right-text" href="/users">Users</a>
+    <a class="right-text" href="/posts">Posts</a>
+
+<a class="right-text" href="/login">Login</a>
+ <a class="right-text" href="/register">Register</a>
+ <a class="right-text" href="https://www.github.com/shawn-dsilva/boomer-php-mvc">Source on Github <i class="fab fa-github"></i></a>
+
 </div>
 
 </nav>
-<h1>Welcome to Boomer MVC Homepage</h1>
+<div class="main">
+  <div class="intro">
+  <h1>BOOMER</h1>
+  <h2>A PHP MVC Framework from Scratch.<br> No Libraries. No Dependencies. Just Pure PHP</h2>
+  </div>
+
+</div>
 <?php require('includes/header.html'); ?>
