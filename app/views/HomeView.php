7d95d1d50ed2924e0e891a143f582d7be3f9e9f0
shawn-dsilva <shawn.dsilva_97@protonmail.com>
2020-03-19 19:42:38 +0530

    homepage front end start

--- app/views/HomeView.php
+++ app/views/HomeView.php
@@ -1,5 +1,14 @@
 <?php require('includes/header.html'); ?>
 
+<nav class="navbar">
+  <a class="logo" href="#home">BOOMER</a>
+
+  <div class="right-container">
+    <a class="right-text" href="https://www.github.com/shawn-dsilva/boomer-php-mvc">Source on Github</a>
+<a class="right-text" href="/login">Login.</a>
+ <a class="right-text" href="/register">Register.</a>
+</div>
+
+</nav>
 <h1>Welcome to Boomer MVC Homepage</h1>
-<a href="/login">Login.</a> &nbsp; &nbsp; <a href="/register">Register.</a>
 <?php require('includes/header.html'); ?>
