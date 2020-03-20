6ab2ca02e7118c3a7aef0da584be054371f74946
shawn-dsilva <shawn.dsilva_97@protonmail.com>
2020-03-20 16:05:01 +0530

    minor fixes

--- app/views/HomeView.php
+++ app/views/HomeView.php
@@ -1,48 +1,47 @@
 <?php require('includes/header.html'); ?>
 
-<nav class="navbar">
-  <a class="logo" href="#home">BOOMER</a>
+  <nav class="navbar">
+    <a class="logo" href="#home">BOOMER</a>
 
-  <div class="right-container">
-    <a class="right-text" href="/users">Users</a>
-    <a class="right-text" href="/posts">Posts</a>
+    <div class="right-container">
+      <a class="right-text" href="/users">Users</a>
+      <a class="right-text" href="/posts">Posts</a>
+      <a class="right-text" href="/login">Login</a>
+      <a class="right-text" href="/register">Register</a>
+      <a class="right-text" href="https://www.github.com/shawn-dsilva/boomer-php-mvc">Source on Github <i class="fab fa-github"></i></a>
 
-<a class="right-text" href="/login">Login</a>
- <a class="right-text" href="/register">Register</a>
- <a class="right-text" href="https://www.github.com/shawn-dsilva/boomer-php-mvc">Source on Github <i class="fab fa-github"></i></a>
+    </div>
 
-</div>
+  </nav>
 
-</nav>
 <div class="main">
   <div class="intro-container">
   <div class="intro">
   <h1>BOOMER</h1>
   <h2>A PHP MVC Framework from Scratch.<br> No Libraries. No Dependencies. Just Pure PHP</h2>
   </div>
-  <div class="intro-content">
-    <div class="intro-content-text">
-    <h2>ABOUT</h2>
-    <p>Boomer PHP MVC is a Framework made by 「 Shawn D'silva 」to learn PHP and
-      core web development and software engineering concepts like Object Oriented programming, Model-View-Controller architecture, AJAX programming etc</p>
-    <p>Without the aid of a pre-existing framework like Laravel, or any external packages</p>
-    <p>BOOMER is built with the classic <b>L</b>inux, <b>A</b>pache, <b>M</b>ySQL, <b>P</b>HP  stack inside Docker containers on an Ubuntu host</p>
-    <p>On the Front-End BOOMER uses JQuery, SASS and CSS3 FlexBox with AJAX calls wherever required, The CSS is all custom with no Bootstrap etc used</p>
-    <p class="stack">
-    <img src="http://konpa.github.io/devicon/devicon.git/icons/linux/linux-original.svg">
-    <img src="http://konpa.github.io/devicon/devicon.git/icons/apache/apache-original-wordmark.svg">
-    <img src="http://konpa.github.io/devicon/devicon.git/icons/mysql/mysql-original-wordmark.svg">
-    <img src="http://konpa.github.io/devicon/devicon.git/icons/php/php-plain.svg">
-    <img src="http://konpa.github.io/devicon/devicon.git/icons/docker/docker-original-wordmark.svg">
-    <img src="http://konpa.github.io/devicon/devicon.git/icons/ubuntu/ubuntu-plain-wordmark.svg">
+    <div class="intro-content">
+      <div class="intro-content-text">
+        <h2>ABOUT</h2>
+        <p>Boomer PHP MVC is a Framework made by 「 <a href="https://shawndsilva.com">Shawn D'silva</a> 」to learn PHP and
+          core web development and software engineering concepts like Object Oriented programming, Model-View-Controller architecture, AJAX programming etc</p>
+        <p>Without the aid of a pre-existing framework like Laravel, or any external packages</p>
+        <p>BOOMER is built with the classic <b>L</b>inux, <b>A</b>pache, <b>M</b>ySQL, <b>P</b>HP  stack inside Docker containers on an Ubuntu host</p>
+        <p>On the Front-End BOOMER uses JQuery, SASS and CSS3 FlexBox with AJAX calls wherever required, The CSS is all custom with no Bootstrap etc used</p>
+        <p class="stack">
+        <img src="http://konpa.github.io/devicon/devicon.git/icons/linux/linux-original.svg">
+        <img src="http://konpa.github.io/devicon/devicon.git/icons/apache/apache-original-wordmark.svg">
+        <img src="http://konpa.github.io/devicon/devicon.git/icons/mysql/mysql-original-wordmark.svg">
+        <img src="http://konpa.github.io/devicon/devicon.git/icons/php/php-plain.svg">
+        <img src="http://konpa.github.io/devicon/devicon.git/icons/docker/docker-original-wordmark.svg">
+        <img src="http://konpa.github.io/devicon/devicon.git/icons/ubuntu/ubuntu-plain-wordmark.svg">
 
-    <img src="http://konpa.github.io/devicon/devicon.git/icons/jquery/jquery-original-wordmark.svg">
-    <img src="http://konpa.github.io/devicon/devicon.git/icons/sass/sass-original.svg">
-
-</p>
-</div>
+        <img src="http://konpa.github.io/devicon/devicon.git/icons/jquery/jquery-original-wordmark.svg">
+        <img src="http://konpa.github.io/devicon/devicon.git/icons/sass/sass-original.svg">
+        </p>
+      </div>
+    </div>
   </div>
-  </div>
 
 </div>
-<?php require('includes/header.html'); ?>
+<?php require('includes/footer.html'); ?>
