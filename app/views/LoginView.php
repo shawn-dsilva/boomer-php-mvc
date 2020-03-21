2799a8a51633cab1b733f584b6637fd0e716e53f
shawn-dsilva <shawn.dsilva_97@protonmail.com>
2020-03-21 21:13:20 +0530

    basic form design

--- app/views/LoginView.php
+++ app/views/LoginView.php
@@ -1,13 +1,16 @@
 <?php require('includes/header.html'); ?>
 <?php require('includes/navbar.html'); ?>
-
+<div class="container">
+  <div class="card">
 <h1>LOGIN</h1>
 
-<form id="login" >
-E-Mail: <input type="text" name="email" id="email"><br>
-Password: <input type="password" name="password" id="password"><br>
-<input type="submit">
-</form>
-<div id="errorBox"></div>
+    <form id="login" >
+    <input type="text" name="email" id="email" placeholder="Enter your E-Mail">
+    <input type="password" name="password" id="password" placeholder="Enter your Password"><br>
+    <input type="submit">
+    </form>
+    <div id="errorBox"></div>
+  </div>
+</div>
 <script>ajaxSubmit('login')</script>
 <?php require('includes/footer.html'); ?>
