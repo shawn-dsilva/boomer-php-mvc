e95b202886c09da59f752f1e51fc15709344cf71
shawn-dsilva <shawn.dsilva_97@protonmail.com>
2020-03-21 21:52:23 +0530

    form rework done

--- app/views/LoginView.php
+++ app/views/LoginView.php
@@ -5,9 +5,17 @@
 <h1>LOGIN</h1>
 
     <form id="login" >
-    <input type="text" name="email" id="email" placeholder="Enter your E-Mail">
-    <input type="password" name="password" id="password" placeholder="Enter your Password"><br>
-    <input type="submit">
+      <div class="input-container">
+      <i class="fas fa-envelope input-icon"></i>
+        <input type="text" name="email" id="email" placeholder="Enter your E-Mail">
+      </div>
+      <div class="input-container">
+      <i class="fas fa-key input-icon"></i>
+        <input type="password" name="password" id="password" placeholder="Enter your Password">
+      </div>
+      <div class="input-container">
+        <button class="submit" type="submit"> Log In <i class="far fa-arrow-alt-circle-right"></i></button>
+      </div>
     </form>
     <div id="errorBox"></div>
   </div>
