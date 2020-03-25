5ce66a0378d0d1941c11c93600022c89f6c7d1f8
shawn-dsilva <shawn.dsilva_97@protonmail.com>
2020-03-25 23:36:12 +0530

    DB setup fixed

--- bin/db.sql
+++ bin/db.sql
@@ -16,17 +16,17 @@ CREATE TABLE `posts` (
   `user_id` int(11) unsigned NOT NULL,
   `title` varchar(100) NOT NULL,
   `content` varchar(600) NOT NULL,
-  `created_at` datetime NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `sessions`;
 
 CREATE TABLE `sessions` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `sessionId` varchar(255) unsigned NOT NULL,
+  `id` int(11) unsigned NOT NULL,
+  `sessionId` varchar(255) NOT NULL,
   `email` varchar(100) NOT NULL,
   `username` varchar(50) NOT NULL,
-  `created_at` datetime NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`sessionId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
