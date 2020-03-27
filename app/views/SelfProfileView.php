c34b0fb3804cc59f0fb1b37ddc118d6b1e6734a4
shawn-dsilva <shawn.dsilva_97@protonmail.com>
2020-03-27 17:17:21 +0530

    user profile basic styling 2

--- app/views/SelfProfileView.php
+++ app/views/SelfProfileView.php
@@ -8,33 +8,36 @@
 
     <div class="profile-data">
 
+    <!-- <?php echo empty($data['user_data']['name']) ? "None" : "<div class='profile-item-content'>{$data['user_data']['username']}</div> "  ?> -->
+
+
     <div class="profile-item">
       <div class="profile-item-title"><i class="far fa-id-card"></i>
-      Name </div> <?php echo $data['user_data']['name']; ?>
+      Name </div>     <?php echo empty($data['user_data']['name']) ? "<div class='profile-item-content'>None</div>" : "<div class='profile-item-content'>{$data['user_data']['name']}</div> "  ?>
       </div>
 
       <div class="profile-item">
 
       <div class="profile-item-title"><i class="fas fa-user"></i>
-      Username </div><?php echo "<div class='profile-item-content'>{$data['user_data']['username']}</div> "?>
+      Username </div>    <?php echo empty($data['user_data']['username']) ? "<div class='profile-item-content'>None</div>" : "<div class='profile-item-content'>{$data['user_data']['username']}</div> "  ?>
       </div>
 
       <div class="profile-item">
 
       <div class="profile-item-title"><i class="fas fa-envelope"></i>
-      E-Mail </div><?php echo "<div class='profile-item-content'>{$data['user_data']['email']}</div> "?>
+      E-Mail </div>    <?php echo empty($data['user_data']['email']) ? "<div class='profile-item-content'>None</div>" : "<div class='profile-item-content'>{$data['user_data']['email']}</div> "  ?>
       </div>
 
       <div class="profile-item">
 
       <div class="profile-item-title"><i class="fas fa-info-circle"></i>
-      About </div><?php echo $data['user_data']['about']; ?>
+      About </div>    <?php echo empty($data['user_data']['about']) ? "<div class='profile-item-content'>None</div>" : "<div class='profile-item-content'>{$data['user_data']['about']}</div> "  ?>
       </div>
 
       <div class="profile-item">
 
       <div class="profile-item-title"><i class="fas fa-map-marker-alt"></i></i>
-      Location </div> <?php echo $data['user_data']['location']; ?>
+      Location </div>    <?php echo empty($data['user_data']['location']) ? "<div class='profile-item-content'>None</div>" : "<div class='profile-item-content'>{$data['user_data']['location']}</div> "  ?>
       </div>
 
 </div>
