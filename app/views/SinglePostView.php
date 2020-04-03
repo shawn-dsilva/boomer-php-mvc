3b65610415a5e20a65736fc6ee829ad494f6b929
shawn-dsilva <shawn.dsilva_97@protonmail.com>
2020-04-03 14:27:29 +0530

    basic single post display and comments css doen

--- app/views/SinglePostView.php
+++ app/views/SinglePostView.php
@@ -2,12 +2,16 @@
 <?php require('includes/navbar.php'); ?>
 
 
-<div id="onePost">
+<div class="post-container" id="onePost">
+  <div class="post-content">
   <span hidden id="id"><?php echo $data['id']; ?></span>
-  <h2 id="title"><?php echo $data['title']; ?></h2>
+  <h1 id="title"><?php echo $data['title']; ?></h1>
   <p id="content"><?php echo $data['content']; ?></p>
-  <button onclick="javascript:openEditBox()">Edit</button>
+  <p><button onclick="javascript:openEditBox()">Edit</button></p>
 
+  <h1>Comments</h1>
+  <p><textarea rows="10" columns="30" placeholder="Add a Comment!"></textarea><button>Submit Comment</button></p>
+  </div>
 </div>
 <div id="editBox"></div>
 <?php require('includes/footer.html'); ?>
