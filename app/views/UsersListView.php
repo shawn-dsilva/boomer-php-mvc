d424ea85b80122dd1359ccaab91a32707fdc9851
shawn-dsilva <shawn.dsilva_97@protonmail.com>
2020-04-10 11:19:56 +0530

    users list css style start

--- app/views/UsersListView.php
+++ app/views/UsersListView.php
@@ -1,13 +1,13 @@
 <?php require('includes/header.html'); ?>
 <?php require('includes/navbar.php'); ?>
-
+<div class="userlist-bg">
 <div class="userlist-container">
   <h1>Registered Users</h1>
 <?php foreach ($data as $item): ?>
-  <div id="userlist-item">
+  <div class="userlist-item">
     <h2><?php echo $item['name']." ( ".$item['username']." ) " ?></h2>
   </div>
 <?php endforeach; ?>
 </div>
-
+</div>
 <?php require('includes/footer.html'); ?>
