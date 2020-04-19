0031c2d65bd10ed3b9dc9baa5b136eb2e2e4c80a
shawn-dsilva <shawn.dsilva_97@protonmail.com>
2020-04-19 16:06:14 +0530

    Middlewares refactored

--- core/middleware/Middlewares.php
+++ core/middleware/Middlewares.php
@@ -1,28 +1,4 @@
 <?php
 
-function isAuth($obj) {
-
-    if(!isset($_COOKIE['sessionId']) ) {
-
-      if ($obj->uri == 'dashboard' || $obj->uri == 'profile' || $obj->uri == 'createpost' || $obj->uri == 'postlist') {
-         Header("Location: login");
-        exit();
-
-      }
-
-      return [false, 'someText'];
-
-    } else {
-
-      $userdata = sessionUserData($_COOKIE['sessionId']);
-
-      if($obj->uri == 'login' || $obj->uri == 'register') {
-        Header("Location: dashboard");
-        exit();
-      }
-
-      return [true, 'user_data' => $userdata];
-
-    }
-
-}
+include_once "../core/middleware/isAuth.php";
+include_once "../core/middleware/inputValidate.php";
