5cad92c67f1741aca931010a7b68bdae8693ff53
shawn-dsilva <shawn.dsilva_97@protonmail.com>
2020-04-25 19:35:14 +0530

    docker-compose now uses php to init db with tables

--- bin/setup.php
+++ bin/setup.php
@@ -22,7 +22,52 @@ define('OPTS', [
 
         // USE ${DB_NAME};";
 
-        $sql = file_get_contents('bin/db.sql');
+        $sql ="
+        DROP TABLE IF EXISTS `users`;
+        
+        CREATE TABLE `users` (
+          `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+          `username` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+          `email` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+          `password` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+          `name` varchar(100) COLLATE utf8_unicode_ci ,
+          `about` varchar(1200) COLLATE utf8_unicode_ci ,
+          `location` varchar(100) COLLATE utf8_unicode_ci,
+          `registered_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        
+          PRIMARY KEY (`id`)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+        
+        DROP TABLE IF EXISTS `posts`;
+        
+        CREATE TABLE `posts` (
+          `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+          `user_id` int(11) unsigned NOT NULL,
+          `title` varchar(100) NOT NULL,
+          `content` varchar(600) NOT NULL,
+          `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+          PRIMARY KEY (`id`)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+        
+        DROP TABLE IF EXISTS `sessions`;
+        
+        CREATE TABLE `sessions` (
+          `id` int(11) unsigned NOT NULL,
+          `sessionId` varchar(255) NOT NULL,
+          `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+          PRIMARY KEY (`sessionId`)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+        
+        DROP TABLE IF EXISTS `comments`;
+        
+        CREATE TABLE `comments` (
+          `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+          `post_id` int(11) unsigned NOT NULL,
+          `user_id` int(11) unsigned NOT NULL,
+          `content` varchar(500) NOT NULL,
+          `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+          PRIMARY KEY (`id`)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
 
         //die(var_dump($sql));
         $db->exec($sql);
