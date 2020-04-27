6fdff62e8fd8f6acad7b9879138a309d17ebfc7c
shawn-dsilva <shawn.dsilva_97@protonmail.com>
2020-04-27 20:54:29 +0530

    changes for prod ready confirmed working

--- public/index.php
+++ public/index.php
@@ -4,7 +4,6 @@ require '../core/router/Router.php';
 require '../core/utils/utilities.php';
 
 function purify($uri) {
-<<<<<<< HEAD
  // die(var_dump(strpos($uri, "boomer-php-mvc/")));
   if(strpos($uri, "boomer-php-mvc/") === 0) {
   $uri = str_replace('boomer-php-mvc/','',$uri);
@@ -12,13 +11,6 @@ function purify($uri) {
   	$uri = "";
   }
 // die(var_dump($uri));
-=======
-  // die(var_dump(findURI()));
-  if(strpos($uri, "boomer-php-mvc/") === 0) {
-  $uri = str_replace('boomer-php-mvc/','',$uri);
-  }
-    // die(var_dump($uri));
->>>>>>> 5f7955a068209f41c8661ebb872cf8cdb4d03291
   return $uri;
 }
 
